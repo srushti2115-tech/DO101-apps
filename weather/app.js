7ff648b96c78ce78b40658f7a396b475a7ca8cc9
srushti.dhumal23@pcu.edu.in <srushti.dhumal23@gmail.com>
2025-01-24 22:54:30 +0530

    updated app to weather

--- weather/app.js
+++ weather/app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
 
 var app = express();
 
